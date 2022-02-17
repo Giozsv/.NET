ed07d9c67ed017c22177d12c19496c7e6700756b
Giovanna Barbosa <96540576+Giozsv@users.noreply.github.com>
2022-02-17 14:13:20 -0300

    Create ExemploConstrutores.cs

---
+++ ExemploConstrutores.cs
@@ -0,0 +1,22 @@
+
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.30114.105
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "ExemploConstrutores", "ExemploConstrutores\ExemploConstrutores.csproj", "{A6D356FF-BDD2-422B-9787-F7D34FF2B125}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{A6D356FF-BDD2-422B-9787-F7D34FF2B125}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{A6D356FF-BDD2-422B-9787-F7D34FF2B125}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{A6D356FF-BDD2-422B-9787-F7D34FF2B125}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{A6D356FF-BDD2-422B-9787-F7D34FF2B125}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+EndGlobal
